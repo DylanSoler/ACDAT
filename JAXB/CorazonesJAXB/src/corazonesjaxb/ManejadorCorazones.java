b663795036b3345c79ec9c9b4c6bc590893fbc42
Unknown <dylanadriansoler@gmail.com>
2018-12-14 20:30:24 +0100

    Reorganizado

---
+++ JAXB/CorazonesJAXB/src/corazonesjaxb/ManejadorCorazones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corazonesjaxb;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author dasoler
+ */
+public class ManejadorCorazones {
+    
+
+    
+}
