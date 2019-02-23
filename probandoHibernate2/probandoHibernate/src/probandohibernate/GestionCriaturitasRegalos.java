e6eceb3e73a87a224c0f609eab1cf892bdabda26
Unknown <dylanadriansoler@gmail.com>
2019-02-23 10:27:59 +0100

    casi acabado hibernate

---
+++ probandoHibernate2/probandoHibernate/src/probandohibernate/GestionCriaturitasRegalos.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probandohibernate;
+
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author dasoler
+ */
+public class GestionCriaturitasRegalos 
+{ 
+    
+  public CriaturitaConRegalos obtenerCriaturita(Session sss, byte id)
+  {
+    Query query = sss.createQuery("from CriaturitaConRegalos where id= :id");
+    query.setByte("id", id);
+    CriaturitaConRegalos cr = (CriaturitaConRegalos)query.uniqueResult();
+    return cr;
+  }
+  
+  
+  public void quitarRegaloCriaturita (Session ses, CriaturitaConRegalos c ,int id)
+  {
+      Transaction t = ses.beginTransaction();
+      
+      Boolean salir = false;
+      for(int i=0; i<c.getRegalitos().size() && !salir;i++)
+      {
+          RegaloParaCriaturitaConRegalos r = c.getRegalitos().get(i);
+          if(r.getId()==id) {
+              c.getRegalitos().remove(i);
+              salir = true;
+          }
+      }
+
+      Query query = ses.createQuery("from RegaloParaCriaturitaConRegalos where id= :id");
+      query.setInteger("id", id);
+      RegaloParaCriaturitaConRegalos reg = (RegaloParaCriaturitaConRegalos)query.uniqueResult();
+      
+      reg.setPropietario(null);
+      
+      t.commit();
+  }
+    
+  
+  public void asignarRegaloCriaturita (Session ses, byte idCriatura , int idRegalo)
+  {
+      Transaction t = ses.beginTransaction();
+      
+      Query query = ses.createQuery("from CriaturitaConRegalos where id= :id");
+      query.setInteger("id", idCriatura);
+      CriaturitaConRegalos criatura = (CriaturitaConRegalos)query.uniqueResult();
+      
+      query = ses.createQuery("from RegaloParaCriaturitaConRegalos where id= :id");
+      query.setInteger("id", idRegalo);
+      RegaloParaCriaturitaConRegalos reg = (RegaloParaCriaturitaConRegalos)query.uniqueResult();
+      
+      criatura.getRegalitos().add(reg);
+      
+      reg.setPropietario(criatura);
+      
+      t.commit();
+  }
+    
+  public void insertarCriaturita(Session ses, Criaturitas c)
+  {
+    Transaction tx = ses.beginTransaction();
+
+    ses.save(c);
+            
+    tx.commit();
+  }
+  
+  public void insertarRegalo(Session ses, RegaloParaCriaturitaConRegalos r)
+  {
+    Transaction tx = ses.beginTransaction();
+
+    ses.save(r);
+            
+    tx.commit();
+  }
+  
+  public void eliminarRegalo(Session ses, int idRegalo)
+  {
+    Transaction tx = ses.beginTransaction();
+
+    RegaloParaCriaturitaConRegalos r = (RegaloParaCriaturitaConRegalos)ses.get(RegaloParaCriaturitaConRegalos.class,idRegalo);
+    ses.delete(r);
+    
+    tx.commit();
+  }
+  
+  public void eliminarCriaturita(Session ses, byte id)
+  {
+    Transaction tx = ses.beginTransaction();
+
+    Query query = ses.createQuery("from RegaloParaCriaturitaConRegalos where GoesTo= :idPropietario");
+    query.setInteger("idPropietario", id);
+    List<RegaloParaCriaturitaConRegalos> listadoRegalosCriaturita = (List<RegaloParaCriaturitaConRegalos>)query.list();
+    
+    for(RegaloParaCriaturitaConRegalos r:listadoRegalosCriaturita)
+    {
+        r.setPropietario(null);
+    }
+    
+    query = ses.createQuery("from CriaturitaConRegalos where id= :id");
+    query.setInteger("id", id);
+    CriaturitaConRegalos cr = (CriaturitaConRegalos)query.uniqueResult();
+    
+    List<Cuento> cuentos = cr.getListaCuentos();
+    
+    for(Cuento c: cuentos)
+    {
+        for(int i=0;i<c.getListaLectores().size();i++)
+        {
+           if(c.getListaLectores().get(i).getId()==id){
+               c.getListaLectores().remove(i);
+           }
+        }
+    }
+    
+    Criaturitas c = (Criaturitas)ses.get(Criaturitas.class,id);
+    ses.delete(c);
+    
+    tx.commit();
+  }
+  
+  public Cuento obtenerCuento(Session sss, int id)
+  {
+    Query query = sss.createQuery("from Cuento where Id= :id");
+    query.setInteger("id", id);
+    Cuento c = (Cuento)query.uniqueResult();
+    return c;
+  }
+}
---
+++ probandoHibernate2/probandoHibernate/src/probandohibernate/ProbandoHibernate.java
@@ -0,0 +1,321 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probandohibernate;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author dasoler
+ */
+public class ProbandoHibernate {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    private static SessionFactory sf = null;
+    
+    public static void main(String[] args) {
+        
+        Session session = null;
+        GestionCriaturitasRegalos gest = new GestionCriaturitasRegalos();
+
+        int opcion=0;
+        Scanner tec = new Scanner(System.in);
+        byte id;
+        int idRegalo;
+        String denominacion;
+        int ancho;
+        int largo;
+        int alto;
+        char tipo;
+        int edadMinima;
+        BigDecimal precio;
+        List<RegaloParaCriaturitaConRegalos> listadoR;
+        List<Cuento> listadoCuentos;
+        List<Criaturitas> listadoC;
+        int idCuento;
+        
+      do{
+        
+        do{ 
+            System.out.println("Elige una opcion o cero para salir");
+            menu();
+            opcion = tec.nextInt();
+        }while(opcion<0 || opcion>16);
+        
+        if(opcion!=0)
+        {
+            sf = EjemplosSessionFactory.getSessionFactory();
+            session = sf.openSession();
+            
+            switch(opcion)
+            {
+                case 1: //Listar todas las criaturitas
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        
+                        for(Criaturitas c:listadoC)
+                            {System.out.println("ID: "+c.getId()+" Nombre:"+c.getNombre());}
+                    break;
+                    
+                case 2: //Listar todos los regalos
+                        listadoR = (List<RegaloParaCriaturitaConRegalos>)session.createQuery("from RegaloParaCriaturitaConRegalos").list();
+                        
+                        for(RegaloParaCriaturitaConRegalos r:listadoR)
+                            {System.out.println(r.toString());}
+                    break;
+                    
+                case 3: //Recuperar una criaturita con todos sus regalos
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        for(Criaturitas c:listadoC)
+                            {System.out.println("ID: "+c.getId()+" Nombre:"+c.getNombre());}
+                        
+                        do{ System.out.println("Introduce el id de la criaturita");
+                            id = tec.nextByte(); }while(id<0);
+
+                        CriaturitaConRegalos cr = gest.obtenerCriaturita(session, id);
+                        
+                        System.out.println(cr.toString());
+                        for(RegaloParaCriaturitaConRegalos r:cr.getRegalitos())
+                            {System.out.println(r.toString());}                     
+                    break;
+                    
+                case 4: //Quitar un regalo a una criaturita (sin borrarlo)
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        for(Criaturitas c:listadoC)
+                            {System.out.println("ID: "+c.getId()+" Nombre:"+c.getNombre());}
+                        
+                        do{ System.out.println("Introduce el id de la criaturita");
+                            id = tec.nextByte(); }while(id<0);
+
+                        CriaturitaConRegalos criatura = gest.obtenerCriaturita(session, id);
+                        
+                        System.out.println(criatura.toString());
+                        for(RegaloParaCriaturitaConRegalos r:criatura.getRegalitos())
+                            {System.out.println(r.toString());} 
+                        
+                        do{
+                            System.out.println("Que regalo quieres quitarle? Introduce el id");
+                            idRegalo = tec.nextInt();
+                        }while(idRegalo<0);
+                        
+                        gest.quitarRegaloCriaturita(session, criatura, idRegalo);
+                        System.out.println("Borrado con éxito");
+                        
+                    break;
+                    
+                case 5: //Asignar un regalo a una criaturita (o ninguna)
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        for(Criaturitas c:listadoC)
+                            {System.out.println("ID: "+c.getId()+" Nombre:"+c.getNombre());}
+                        
+                        do{ System.out.println("Introduce el id de la criaturita");
+                            id = tec.nextByte(); }while(id<0);
+                        
+                        listadoR = (List<RegaloParaCriaturitaConRegalos>)session.createQuery("from RegaloParaCriaturitaConRegalos").list();
+                        
+                        for(RegaloParaCriaturitaConRegalos r:listadoR)
+                            {
+                                if(r.getPropietario()==null)
+                                    System.out.println(r.toString());
+                            }
+                        
+                        do{
+                            System.out.println("Que regalo quieres asignarle? Introduce el id");
+                            idRegalo = tec.nextInt();
+                        }while(idRegalo<0);
+                        
+                        gest.asignarRegaloCriaturita(session, id, idRegalo);
+                        
+                        System.out.println("Asignado!");
+                        
+                    break;
+                    
+                case 6: //Crear una nueva criaturita
+                        String nombre;
+                        
+                        tec.nextLine();
+                        
+                        System.out.println("Introduce el nombre:");
+                        nombre = tec.nextLine();
+                        
+                        do{
+                            System.out.println("Introduce el id:");
+                            id = tec.nextByte();
+                        }while(id<0);
+     
+                        tec.nextLine();
+                        
+                        gest.insertarCriaturita(session, new Criaturitas(id, nombre));
+                        System.out.println("Creado con exito");
+                        
+                    break;
+                    
+                case 7: //Crear un nuevo regalo
+                        do{
+                            System.out.println("Introduce el id:");
+                            idRegalo = tec.nextInt();
+                        }while(idRegalo<0);
+                        tec.nextLine();
+                        System.out.println("Denominacion:");
+                        denominacion = tec.nextLine();
+                        do{
+                            System.out.println("Ancho:");
+                            ancho = tec.nextInt();
+                        }while(ancho<0);
+                        do{
+                            System.out.println("Largo:");
+                            largo = tec.nextInt();
+                        }while(largo<0);
+                        do{
+                            System.out.println("Alto:");
+                            alto = tec.nextInt();
+                        }while(alto<0);
+                        System.out.println("Tipo:");
+                        tipo = tec.next().charAt(0);
+                        do{
+                            System.out.println("Edad minima:");
+                            edadMinima = tec.nextInt();
+                        }while(ancho<0);
+                        do{
+                            System.out.println("Precio:");
+                            precio = tec.nextBigDecimal();
+                        }while(ancho<0);
+                        tec.nextLine();
+                        
+                        RegaloParaCriaturitaConRegalos regalazo = new RegaloParaCriaturitaConRegalos(idRegalo,denominacion,ancho,largo,alto,tipo,edadMinima,precio,null);
+                        gest.insertarRegalo(session, regalazo);
+                        System.out.println("Creado con exito");
+                        
+                    break;
+                    
+                case 8: //Borrar un regalo
+                        listadoR = (List<RegaloParaCriaturitaConRegalos>)session.createQuery("from RegaloParaCriaturitaConRegalos").list();
+                        for(RegaloParaCriaturitaConRegalos r:listadoR){System.out.println(r.toString());}    
+                        
+                        do{
+                            System.out.println("\nIntroduce el id:");
+                            idRegalo = tec.nextInt();
+                        }while(idRegalo<0);
+                        
+                        gest.eliminarRegalo(session, idRegalo);
+                        System.out.println("Regalo borrado");
+                        
+                    break;
+                    
+                case 9: //Borrar una criaturita y todos sus regalos (POR PROBAR AUN)
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        for(Criaturitas c:listadoC){System.out.println("ID: "+c.getId()+" Nombre:"+c.getNombre());}
+                        
+                        do{
+                            System.out.println("\nIntroduce el id:");
+                            id = tec.nextByte();
+                        }while(id<0);
+                        
+                        gest.eliminarCriaturita(session, id);
+                        System.out.println("Eliminada con exito");
+                                
+                    break;
+                    
+                case 10: //Listar todos los cuentos
+                        listadoCuentos = (List<Cuento>)session.createQuery("from Cuento").list();
+                        
+                        for(Cuento c:listadoCuentos)
+                            {System.out.println("ID: "+c.getId()+"\nTitulo: "+c.getTitulo()+"\nAutor:"+c.getAutor()+"\nTema: "+c.getTema());}
+                    break;
+                    
+                case 11: //Recuperar una criaturita con todos sus cuentos
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        for(Criaturitas c:listadoC)
+                            {System.out.println("ID: "+c.getId()+" Nombre:"+c.getNombre());}
+                        
+                        do{ System.out.println("Introduce el id de la criaturita");
+                            id = tec.nextByte(); }while(id<0);
+
+                        cr = gest.obtenerCriaturita(session, id);
+                        
+                        System.out.println(cr.toString());
+                        for(Cuento c:cr.getListaCuentos())
+                            {System.out.println(c.toString());}
+                    break;
+                    
+                case 12: //Recuperar un cuento y su lista de lectoras
+                        listadoCuentos = (List<Cuento>)session.createQuery("from Cuento").list();
+                        for(Cuento c:listadoCuentos)
+                            {System.out.println("ID: "+c.getId()+"\nTitulo: "+c.getTitulo());}
+                        do{
+                            System.out.println("\nIntroduce el id del cuento: ");
+                            idCuento = tec.nextInt();
+                        }while(idCuento<0);
+                        
+                        Cuento c = gest.obtenerCuento(session, idCuento);
+                        
+                        System.out.println(c.toString());
+                        for(CriaturitaConRegalos criat : c.getListaLectores())
+                        {
+                            System.out.println(criat.toString());
+                        }
+                    break;
+
+                case 13: //Quitar un cuento a una criaturita (sin borrarlo)
+                        listadoC = (List<Criaturitas>)session.createQuery("from Criaturitas").list();
+                        for(Criaturitas cri:listadoC)
+                            {System.out.println("ID: "+cri.getId()+" Nombre:"+cri.getNombre());}
+                        
+                        do{ System.out.println("Introduce el id de la criaturita");
+                            id = tec.nextByte(); }while(id<0);
+
+                        criatura = gest.obtenerCriaturita(session, id);
+                        
+                        System.out.println(criatura.toString());
+                        //por aqui-------
+                        for(RegaloParaCriaturitaConRegalos r:criatura.getRegalitos())
+                            {System.out.println(r.toString());} 
+                        
+                        do{
+                            System.out.println("Que regalo quieres quitarle? Introduce el id");
+                            idRegalo = tec.nextInt();
+                        }while(idRegalo<0);
+                        
+                        gest.quitarRegaloCriaturita(session, criatura, idRegalo);
+                        System.out.println("Borrado con éxito");
+                    break;                    
+            }
+            
+        }
+      }while(opcion!=0);
+        
+      if(session!=null)
+        session.close();
+    }
+    
+    private static void menu()
+    {
+        System.out.println("-----------------------Menu-----------------------");
+        System.out.println("1- Listar todas los criaturitas");
+        System.out.println("2- Listar todos los regalos");
+        System.out.println("3- Recuperar una criaturita");
+        System.out.println("4- Quitar un regalo a una criaturita");
+        System.out.println("5- Asignar un regalo a una criaturita");
+        System.out.println("6- Crear una nueva criaturita");
+        System.out.println("7- Crear un nuevo regalo");
+        System.out.println("8- Borrar un regalo");
+        System.out.println("9- Borrar una criaturita y todos sus regalos");
+        System.out.println("10- Listar todos los cuentos");
+        System.out.println("11- Recuperar una criaturita con todos sus cuentos");
+        System.out.println("12- Recuperar un cuento y su lista de lectoras");
+        System.out.println("13- Quitar un cuento a una criaturita");
+        System.out.println("--------------------------------------------------");
+    }
+    
+}
